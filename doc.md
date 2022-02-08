f22e1f2b78562aec87f3861a8f2cc6db98f73b08
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2022-02-08 15:20:52 +0700

    Insert to mongoDB 🏄🏻‍♂️

--- doc.md
+++ doc.md
@@ -1,3 +1,9 @@
 npm init
 
 npm i --s express@4.16.4 mongoose@5.3.4 express-handlebars@3.0.0 body-parser@1.18.3
+
+npm i nodemon
+
+## start
+
+npx nodemon
--- project/models/db.js
+++ project/models/db.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://admin:8A6BNUEVk7GZvF5u@cluster0.wydy7.mongodb.net/EmployeeDB')
+mongoose.connect('mongodb+srv://admin:8A6BNUEVk7GZvF5u@cluster0.wydy7.mongodb.net/EmployeeDB', { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') }
+    else { console.log('Error in DB connection : ' + err) }
+})
+
+require('./employee.model');
